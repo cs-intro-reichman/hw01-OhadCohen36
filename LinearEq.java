472ebd1451720fe05204c76e3d6f03cc5d2c7733
Ohad Cohen <ohad.cohen02@post.runi.ac.il>
2023-12-20 14:14:31 +0200

    Ohad's Homework 1

--- LinearEq.java
+++ LinearEq.java
@@ -10,7 +10,7 @@ public class LinearEq {
 		 double a = Integer.parseInt(args[0]);
 		 double b = Integer.parseInt(args[1]);
 		 double c = Integer.parseInt(args[2]);
-		 System.out.println(a + "*x + " + b + " = " + c);
+		 System.out.println(a + " * x + " + b + " = " + c);
 		 System.out.println("x = " + (c-b)/a);
 	}
 }
